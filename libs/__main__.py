89d679cae6e2688ffedb1f49c455d3351aab071b
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-06-27 17:14:34 +0900

    population density

--- libs/__main__.py
+++ libs/__main__.py
@@ -1,3 +1,19 @@
 import pandas as pd
+import numpy as np
 
-print("import test")
+def read_csv(file_path):
+    return pd.read_csv(file_path,encoding='euc-kr')
+
+def drop_column(df,column):
+    df = df.drop(column)
+    return df
+
+
+
+def main():
+    population_density_csv = "../data/population.csv"
+    population_density = read_csv(population_density_csv)
+    print(population_density)
+
+if __name__ == "__main__":
+    main()
